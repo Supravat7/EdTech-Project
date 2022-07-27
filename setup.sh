bb3fd384d16b3ab4a1f64163c7aefccbb4495c83
Supravat7 <105975632+Supravat7@users.noreply.github.com>
2022-07-27 13:46:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\[client]\n\
-" > ~/.streamlit/config.toml
+port = $PORT\n\
+[client]\n\
+showErrorDetails = false\n\
+" >>~/.streamlit/config.toml
